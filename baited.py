0024a39d7346fbf76422a292a93df4d8fdc3fba1
hashicodes <44235543+hanascodes@users.noreply.github.com>
2020-01-04 12:37:25 +0100

    Add files via upload

---
+++ baited.py
@@ -0,0 +1,43 @@
+import os
+
+# print("Made in autumn 2019 for")
+# previously known as helloSecondWorld.py
+#successfully baited Raki-san via LinkedIn
+# input()
+#
+# print("  ████████████████         █        ")
+# print("           █               █  /     ")
+# print("           █            /  █ /      ")
+# print("██████████████████     /   █/       ")
+# print("        // █          /    █        ")
+# print("       //  █               █        ")
+# print("      //   █\\              █        ")
+# print("     //    █ \\            / \       ")
+# print("    //     █  \\          /   \      ")
+# print("   //      █            /     \     ")
+# print("  //       █        ___/       \___ ")
+
+# print("")
+#
+# input()
+#
+# os.system('cls')
+
+print('You\'re pretty cool')
+
+input()
+
+os.system('cls')
+
+input()
+
+answerInput = str(input("Wanna go out sometime? y/n "))
+
+input()
+
+if answerInput == 'y':
+    print("Cool. ")
+    input()
+else:
+    print("Fine. You're boring. ")
+    input()
